4f43d9fd3ac128c53890e9bd15a2f0637599d4a8
RyanMatthewBriggs <ryan.briggs248@gmail.com>
2022-05-20 15:09:40 +0100

    js bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,22 @@
 // selectors
 const pokeName = document.getElementById('pokeName');
+const pokePageTitle = document.getElementById("pokePageTitle");
+
+// imports
+import capitalise from '../utils/capitalise.js';
+
+// fetch api
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(response => response.json())
-  .then(data => console.log(data));
-
-pokeName.innerText = data['name']
+  .then(data => {
+    // change page title to currently active pokemon
+    let pokeName = data.name;
+    let capPokeName = capitalise(pokeName);
+    console.log(capPokeName);
+    pokeName.innerText = capPokeName;
+    pokePageTitle.innerText = `Pokédex - ${capPokeName}`;
+  })
+  .catch(function(err) {
+    // alert(`we're having technical difficulty, fuck off and come back later`);
+  });
