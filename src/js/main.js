788e8acfb5751989b46f2bb7ccb09ed76dc52fd2
RyanMatthewBriggs <ryan.briggs248@gmail.com>
2022-05-22 13:08:07 +0100

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,25 @@
 // selectors
-const pokeName = document.getElementById('pokeName');
-const pokePageTitle = document.getElementById("pokePageTitle");
+let pokeName = document.querySelector('.pokeName');
+let pokeType = document.querySelector('.pokeType');
+let pokePageTitle = document.getElementById("pokePageTitle");
+let pokeHeight = document.querySelector('.pokeHeight');
+let pokeWeight = document.querySelector('.pokeWeight');
+let pokeID = document.querySelector('.pokeID');
 
 // imports
 import capitalise from '../utils/capitalise.js';
 
 // fetch api
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+fetch('https://pokeapi.co/api/v2/pokemon/2')
   .then(response => response.json())
-  .then(data => {
+  .then(data => { console.log(data);
     // change page title to currently active pokemon
     let pokeDataName = data.name;
     let capPokeName = capitalise(pokeDataName);
-    pokePageTitle.textContent = `Pokédex - ${capPokeName}`;
-    pokeName.textContent = capPokeName;
+    pokePageTitle.innerText = `Pokédex - ${capPokeName}`;
+    pokeName.innerText = capPokeName;
+    pokeID.innerText = data.id;
   })
   .catch(function(err) {
     // alert(`we're having technical difficulty, fuck off and come back later`);
